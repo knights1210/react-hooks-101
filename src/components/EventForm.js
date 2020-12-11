b6790486e5cdd439708d882d485637f0526f6d06
knights1210 <jamis.1984@gmail.com>
2020-12-11 16:26:34 +0900

    replace type names with constants

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,4 +1,5 @@
 import React, {  useState } from "react";
+import { CREATE_EVENT, DELETE_ALL_EVENTS } from "../actions";
 
 const EventForm = ({state, dispatch}) => {
 
@@ -10,7 +11,7 @@ const EventForm = ({state, dispatch}) => {
     e.preventDefault();
 
     dispatch({
-      type: "CREATE_EVENT",
+      type: CREATE_EVENT,
       title,
       body,
     });
@@ -25,7 +26,7 @@ const EventForm = ({state, dispatch}) => {
     );
     if (result)
       dispatch({
-        type: "DELETE_ALL_EVENTS",
+        type: DELETE_ALL_EVENTS,
       });
   };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-// action = {
+import {CREATE_EVENT, DELETE_ALL_EVENTS, DELETE_EVENT, } from '../actions'
 //   type: 'CREATE_EVENT',
 //   title: '2020東京オリンピックのお知らせ',
 //   body: '2020年に東京でオリンピックを開催します！つきましては、、、、、'
@@ -35,14 +35,14 @@
 
 const events = (state = [], action) => {
   switch (action.type) {
-    case "CREATE_EVENT":
+    case CREATE_EVENT:
       const event = { title: action.title, body: action.body };
       const length = state.length;
       const id = length === 0 ?  1 : state[length - 1].id + 1
       return [...state, { id, ...event }];
-    case "DELETE_EVENT":
+    case DELETE_EVENT:
       return state.filter(event => event.id !== action.id);
-    case "DELETE_ALL_EVENTS":
+    case DELETE_ALL_EVENTS:
       return [];
     default:
       return state;
